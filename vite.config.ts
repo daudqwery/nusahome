0dd6c059a75752658b7e0c4ac0adb690e7216a0d
daudqwery <daudqwerty112@gmail.com>
2026-05-02 00:59:12 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nusahome/",
   server: {
     host: "::",
     port: 8080,
